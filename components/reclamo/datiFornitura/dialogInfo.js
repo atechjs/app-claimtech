96b391bc362f14c2197e33c5922a6823cca64c7c
omar <omar.tor2011@gmail.com>
2023-10-31 12:31:47 +0100

    prendere MP fatture

--- components/reclamo/datiFornitura/dialogInfo.js
+++ components/reclamo/datiFornitura/dialogInfo.js
@@ -5,13 +5,50 @@ import { Button, Stack, Typography } from "@mui/material";
 import LabelInfo from "../LabelInfo";
 import dayjs from "dayjs";
 import DownloadIcon from "@mui/icons-material/Download";
+import GetCurrentAxiosInstance from "../../../utils/Axios";
+import getApiUrl from "../../../utils/BeUrl";
+import { mandaNotifica } from "../../../utils/ToastUtils";
 
 export default function DialogInfo({ opened, handleClose, infoData }) {
   const [open, setOpen] = React.useState(opened);
+  const instance = GetCurrentAxiosInstance();
 
   React.useEffect(() => {
     setOpen(opened);
   }, [opened]);
+
+  const scaricaPdfFattura = () => {
+    const idReclamo = infoData.idReclamo;
+    const nomePdf = infoData.nomePdf;
+    const codiceFattura = infoData.codiceFattura;
+    const url =
+      getApiUrl() +
+      "api/reclamo/pdfFattura?idReclamo=" +
+      idReclamo +
+      "&nomePdf=" +
+      nomePdf;
+    instance({
+      url,
+      method: "GET",
+      responseType: "blob",
+    })
+      .then((response) => {
+        const href = window.URL.createObjectURL(response.data);
+        const anchorElement = document.createElement("a");
+        anchorElement.href = href;
+        anchorElement.download = codiceFattura + ".pdf";
+        document.body.appendChild(anchorElement);
+        anchorElement.click();
+        document.body.removeChild(anchorElement);
+        window.URL.revokeObjectURL(href);
+        mandaNotifica("Fattura scaricata correttamente", "success");
+        handleClose();
+      })
+      .catch((error) => {
+        mandaNotifica("Impossibile generare il report", "error");
+        console.log("error", error);
+      });
+  };
 
   if (infoData == undefined) return;
   return (
@@ -46,7 +83,12 @@ export default function DialogInfo({ opened, handleClose, infoData }) {
           <LabelInfo label={"Qta Kg"} value={infoData.qtaKgFattura} />
         </Stack>
         <LabelInfo label={"Valore"} value={infoData.valoreFattura} />
-        <Button color="error" variant="outlined" startIcon={<DownloadIcon />}>
+        <Button
+          color="error"
+          variant="outlined"
+          startIcon={<DownloadIcon />}
+          onClick={() => scaricaPdfFattura()}
+        >
           Scarica file fattura
         </Button>
       </Stack>
---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "app_claimot",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -- --port 3005",
+    },
+  ],
+};
